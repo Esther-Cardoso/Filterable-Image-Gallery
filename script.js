d375fb3fe2eb6a6ced52d85ed9b44c9b9259e44c
Esther Cardoso <esthercardosofernandes@gmail.com>
2022-04-21 13:30:38 -0300

    Concluido

--- script.js
+++ script.js
@@ -18,4 +18,30 @@ window.onload = () => {
       });
     }
   };
+  for (let index = 0; index < filterImg.length; index++) {
+    filterImg[index].setAttribute("onclick", "preview(this)");
+  }
 };
+
+const previewBox = document.querySelector(".preview-box"),
+  previewImg = previewBox.querySelector("img"),
+  categoryName = previewBox.querySelector(".title p"),
+  closeIcon = previewBox.querySelector(".icon"),
+  shadow = document.querySelector(".shadow");
+
+function preview(element) {
+  document.querySelector("body").style.overflow = "hidden";
+  let selectedPrevImg = element.querySelector("img").src;
+  let selectedImgCategory = element.getAttribute("data-temporadas");
+  categoryName.textContent = selectedImgCategory;
+
+  previewImg.src = selectedPrevImg;
+
+  previewBox.classList.add("show");
+  shadow.classList.add("show");
+  closeIcon.onclick = () => {
+    previewBox.classList.remove("show");
+    shadow.classList.remove("show");
+    document.querySelector("body").style.overflow = "scroll";
+  };
+}
